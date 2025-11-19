c98d58d5b2a5c4407acf8132e62b399b17341af6
nileoe <linoperdrix@gmail.com>
2025-11-19 21:28:55 +0100

    Adds users controller routes definitions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 interface User {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
-const users: User[] = [
-    {id: 1,name:"Lino"}
-]
+let users: User[] = [
+  { id: 1, name: 'Lino' },
+  { id: 2, name: 'Muriel' },
+  { id: 3, name: 'Fred' },
+  { id: 4, name: 'Anne' },
+];
 
 @Controller('users')
 export class UsersController {
-    @Get()
-    findAll(): User[] {
-        return users
-    }
+  @Get()
+  getAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): User[] {
+    console.log(role)
+    return users;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): User {
+    console.log(id);
+    return users[id];
+  }
+
+  @Post()
+  create(@Body() user: any) {
+    console.log(user);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): string {
+    return `deleted user with id ${id}.`;
+  }
 }
